035e59473b571b3d384dace38a6f2bfe76374dbc
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-02-06 13:34:24 +0000

    To the end of Sharing the Environment. Instead of deciding on content view which environment and having to make an instance of NetworkManager in any view that requires networking access we make it in the main app bundle and share it in the environment. This involves work with the keys.

--- NetworkingSandbox/NetworkingSandboxApp.swift
+++ NetworkingSandbox/NetworkingSandboxApp.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 @main
 struct NetworkingSandboxApp: App {
+    //create it once, and no it can't be a StateObject because that requires a class that conforms to the ObservableObject protocol. INterestingly since Paul has never seen the App struct above get destroyed it might be better to just delete the @State but hey.
+    @State var networkManager = NetworkManager(environment: .testing)
+    
+    //he mentioned that soem companies might want to do this:
+    /* if  DEBUG
+     @State var networkManager = NetworkManager(environment: .testing)
+     #else
+     @State var networkManager = NetworkManager(environment: .production)
+     #endIf
+     */
+    //that debug wont even compile without debug mode turned on.
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+            //share it everywhere, now anything inside ContentView or children can now read that networkManager out.
+                .environment(\.networkManager, networkManager)
         }
     }
 }
